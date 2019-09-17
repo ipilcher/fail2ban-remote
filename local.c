ba3d06dbffdf73131941a37c2e9b13c6744edba6
Ian Pilcher <arequipeno@gmail.com>
2019-09-17 18:57:52 -0500

    Add GPLv2 headers

--- local.c
+++ local.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Ian Pilcher <arequipeno@gmail.com>
+ *
+ * This program is free software.  You can redistribute it or modify it under
+ * the terms of version 2 of the GNU General Public License (GPL), as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY -- without even the implied warranty of MERCHANTIBILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the text of the GPL for more details.
+ *
+ * Version 2 of the GNU General Public License is available at:
+ *
+ *   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
 #include <ctype.h>
 #include <errno.h>
 #include <inttypes.h>
